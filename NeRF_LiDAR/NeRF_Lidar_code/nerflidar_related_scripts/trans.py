ebb70a2132b2eb8461d2bc1319317354bde92643
SOTArt <lcp9202@njust.edu.cn>
2024-08-05 18:13:45 +0800

    add test_push_code

--- NeRF_LiDAR/NeRF_Lidar_code/nerflidar_related_scripts/trans.py
+++ NeRF_LiDAR/NeRF_Lidar_code/nerflidar_related_scripts/trans.py
@@ -9,3 +9,4 @@ exps =['weights{}'.format(i) for i in range(1,5)]
 for exp in exps:
     for scene in scenes:
         os.system('cp -r ./Snerf/exp/lidar_rendering_ablation/scene{}/scene{}_{}_lidar_simulation ./simulation_ablation'.format(scene,scene,exp))
+        # 递归复制文件路径，.format中的变量值依次补全{}中的值，构造完整路径
